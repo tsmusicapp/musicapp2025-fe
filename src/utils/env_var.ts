298864822f884f5dab13b4399f7f2ae8ffe9071d
tayyub <tayyub258@gmail.com>
2025-04-24 10:58:40 +0500

    updated code and add new apiurl

--- src/utils/env_var.ts
+++ src/utils/env_var.ts
@@ -1,13 +1,5 @@
-import axios from "axios"
-
 // export const  API_URL = 'http://localhost:5000'
-export const  API_URL =  axios.create({
-    baseURL: 'http://16.171.65.3:5000',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    },
-})
+export const  API_URL = 'http://16.171.65.3:5000'
 
 // For production use this 
 // '34.200.64.144'
