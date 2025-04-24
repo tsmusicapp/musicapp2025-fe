292553c513d66548739e3d87008b29f66bc0cb73
tayyub <tayyub258@gmail.com>
2025-04-24 07:27:01 +0500

    updated code and add new apiurl

--- src/utils/env_var.ts
+++ src/utils/env_var.ts
@@ -1,4 +1,5 @@
-export const  API_URL = 'http://localhost:5000'
+// export const  API_URL = 'http://localhost:5000'
+export const  API_URL = 'http://16.171.65.3:5000'
 
 // For production use this 
 // '34.200.64.144'
