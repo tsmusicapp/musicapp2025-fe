2ba33dfae7fb35abe3a4880b6951ed4e8a8573ef
tsmusicapp <260115905@qq.com>
2025-05-05 00:53:18 +0500

    refactor: update API_URL to use NEXT_PUBLIC_ prefix for environment variables

--- src/utils/env_var.ts
+++ src/utils/env_var.ts
@@ -1,5 +1,5 @@
 // export const  API_URL = 'http://localhost:5000'
-export const API_URL = process.env.API_URL || 'http://localhost:5000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
 // For production use this 
 // '34.200.64.144'
