c047c3790550ee3197ef341c8e0ec8df282d44e9
tsmusicapp <260115905@qq.com>
2025-05-20 12:19:01 +0500

    song display issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,6 @@ export default function Portfolio() {
           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
             {featuredMusic.length > 0 ? (
               featuredMusic
-                .slice(0, 3)
                 .map((props, key) => (
                   <HomeMusicianBox
                     key={key}
