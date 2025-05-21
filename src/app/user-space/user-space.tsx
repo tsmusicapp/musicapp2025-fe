7a4f8c02e4124531fc567d69b22aefb9414ffd1a
tsmusicapp <260115905@qq.com>
2025-05-21 17:36:28 +0500

    my work and my assets

--- src/app/user-space/user-space.tsx
+++ src/app/user-space/user-space.tsx
@@ -1,123 +1,79 @@
 "use client";
 
-import React, { Suspense } from "react";
-import Image from "next/image";
-import { StarIcon } from "@heroicons/react/24/outline";
-import {
-  Card,
-  Input,
-  Checkbox,
-  Button,
-  Typography,
-  Select,
-  Textarea,
-  Avatar,
-  Tab,
-  TabPanel,
-  Tabs,
-  TabsBody,
-  TabsHeader,
-  Spinner,
-} from "@material-tailwind/react";
-import Link from "next/link";
-import { CATEGORIES, JOBS_PROPS } from "@/dummy/example";
-import OriginalBox from "@/components/music-box/original-box";
+import React, { Suspense, useState, useEffect } from "react";
+import { Spinner } from "@material-tailwind/react";
+import { useSearchParams, useRouter } from "next/navigation";
+import MusicPlayerDialog from "@/components/music-player/music-player-dialog";
 import UserInfo from "@/components/user-space/user-info";
 import Works from "@/app/user-space/works";
 import Collections from "@/app/user-space/collections";
 import MyFollowing from "./my-following";
-import { useSearchParams } from "next/navigation";
 import MyAssets from "./my-assets";
-import MusicPlayerDialog from "@/components/music-player/music-player-dialog";
 import MyLiked from "./my-liked";
 
 export function UserSpace() {
   const section = [
-    {
-      label: "Works",
-      value: "works",
-      desc: <Works />,
-    },
-    {
-      label: "My Assets",
-      value: "my-assets",
-      desc: <MyAssets />,
-    },
-    {
-      label: "My following",
-      value: "my-following",
-      desc: <MyFollowing />,
-    },
-    {
-      label: "My Liked",
-      value: "my-liked",
-      desc: <MyLiked/>,
-    },
-    {
-      label: "My Collection",
-      value: "my-collection",
-      desc: <Collections />,
-    },
-    {
-      label: "Draft",
-      value: "draft",
-      desc: "Draft",
-    },
+    { label: "My Works", value: "my-works", desc: <Works /> },
+    { label: "My Assets", value: "my-assets", desc: <MyAssets /> },
+    { label: "My following", value: "my-following", desc: <MyFollowing /> },
+    { label: "My Liked", value: "my-liked", desc: <MyLiked /> },
+    { label: "My Collection", value: "my-collection", desc: <Collections /> },
+    { label: "Draft", value: "draft", desc: <div>Draft</div> },
   ];
-  const searchParams = useSearchParams();
-  let sectionActive = searchParams.get("section");
-  if (sectionActive == null) {
-    sectionActive = "works";
-  }
+
+  const [activeTab, setActiveTab] = useState<string>("my-works");
+
+  const handleTabClick = (value: string) => {
+    setActiveTab(value);
+  };
+
   return (
-    <Suspense fallback={<div><Spinner className="h-12 w-12"/></div>}>
-    <div className="mx-auto min-h-[60rem]" key={sectionActive}>
-      <MusicPlayerDialog />
-      <div className="grid grid-cols-12 pl-10">
-        <UserInfo />
-        <div className="col-start-4 col-span-9 min-h-screen w-full">
-          <div className="p-4">
-            <div className="flex flex-row gap-4 mb-4">
-              <Tabs
-                id="custom-animation"
-                value={sectionActive}
-                className="w-full"
-              >
-                <TabsHeader
-                  className="bg-transparent space-x-2"
-                  indicatorProps={{
-                    className: "bg-gray-900/10 shadow-md !text-gray-900",
-                  }}
-                >
+    <Suspense
+      fallback={
+        <div>
+          <Spinner className="h-12 w-12" />
+        </div>
+      }
+    >
+      <div className="mx-auto min-h-[60rem]" key={activeTab}>
+        <MusicPlayerDialog />
+        <div className="grid grid-cols-12 pl-10">
+          <UserInfo />
+          <div className="col-start-4 col-span-9 min-h-screen w-full">
+            <div className="p-4">
+              <div className="flex flex-row gap-4 mb-4">
+                <div className="flex w-full max-w-full flex-wrap gap-2 border border-white/25 bg-white/10 rounded-md p-2">
                   {section.map(({ label, value }) => (
-                    <Tab
+                    <button
                       key={value}
-                      value={value}
-                      className="font-semibold text-sm w-fit"
+                      onClick={() => handleTabClick(value)}
+                      className={`px-4 py-2 text-sm font-medium rounded-md transition-all duration-300 ${
+                        activeTab === value
+                          ? "bg-gray-900/10 text-gray-900 shadow-md"
+                          : "text-gray-700 hover:bg-gray-100/10"
+                      }`}
                     >
                       {label}
-                    </Tab>
+                    </button>
                   ))}
-                </TabsHeader>
-                <TabsBody
-                  animate={{
-                    initial: { y: 250 },
-                    mount: { y: 0 },
-                    unmount: { y: 250 },
-                  }}
-                >
-                  {section.map(({ value, desc }) => (
-                    <TabPanel key={value} value={value} className="p-0 pt-4">
-                      <div className="max-w-fit">{desc}</div>
-                    </TabPanel>
-                  ))}
-                </TabsBody>
-              </Tabs>
+                </div>
+              </div>
+
+              <div className="border-t-2 border-black/10 w-full mb-2" />
+
+              <div className="w-full max-w-full">
+                {section.map(({ value, desc }) =>
+                  activeTab === value ? (
+                    <div key={value} className="p-0 pt-4 max-w-fit">
+                      {desc}
+                    </div>
+                  ) : null
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </Suspense>
   );
 }
--- src/app/user-space/works.tsx
+++ src/app/user-space/works.tsx
@@ -1,50 +1,55 @@
-// import { CATEGORIES } from "@/dummy/example";
-import { CATEGORIES } from "@/conf/music";
-import React from "react";
-import BoxWithInfo from "../../components/music-box/box-with-info";
+"use client";
+import HomeMusicianBox from "@/components/music-box/home-musician-box";
+import { getAuthToken } from "@/utils/auth";
+import { API_URL } from "@/utils/env_var";
+import { Spinner } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
 
 function Works() {
+  const [myMusic, setMyMusic] = useState<any[]>([]);
+  useEffect(() => {
+    const getMyMusic = async () => {
+      try {
+        const token = getAuthToken();
+
+        const endpoint = `${API_URL}/v1/music/get-music`;
+        const response = await fetch(endpoint, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message || "Failed to fetch music data");
+        }
+
+        const data = await response.json();
+        setMyMusic(data);
+      } catch (error) {
+        console.error("Error fetching music data:", error);
+      }
+    };
+
+    getMyMusic();
+  }, []);
   return (
     <>
-      <div className="grid grid-cols-4 gap-2">
-        {CATEGORIES.map((props, key) => (
-          <BoxWithInfo key={key} withName={false} {...props} withSale={false} />
-        ))}
-      </div>
-      <div className="flex justify-start items-start gap-2">
-        <div className="w-full flex flex-col justify-start items-start font-semibold text-sm py-8">
-          <label
-            htmlFor="dropzone-file"
-            className="flex flex-col items-center justify-center w-[35.9rem] h-[10rem] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
-          >
-            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg
-                className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 16"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                />
-              </svg>
-              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                <span className="font-semibold">Share Your Work</span>
-                {/* or drag
-                    and drop */}
-              </p>
-              {/* <p className="text-xs text-gray-500 dark:text-gray-400">
-                    SVG, PNG, JPG or GIF (MAX. 800x400px)
-                  </p> */}
-            </div>
-            <input id="dropzone-file" type="file" className="hidden" />
-          </label>
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5">
+        {myMusic.length > 0 ? (
+          myMusic.map((props, key) => (
+            <HomeMusicianBox
+              key={key}
+              {...props}
+              source="home"
+              lyrics={false}
+              isMusicAsset={false}
+            />
+          ))
+        ) : (
+          <Spinner className="w-10 h-10 mx-auto" />
+        )}
       </div>
     </>
   );
