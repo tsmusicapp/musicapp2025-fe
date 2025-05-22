a26bf0962daa14056b921bcaf4ac47b0d368d81f
tsmusicapp <260115905@qq.com>
2025-05-22 17:21:13 +0500

    align the lyric

--- src/components/music-player/content-left-side-v2.tsx
+++ src/components/music-player/content-left-side-v2.tsx
@@ -37,16 +37,16 @@ function ContentLeftSideV2({ musicDetailInfo }: ContentLeftSideV2Props) {
             : musicDetailInfo?.tags || "N/A",
         ],
       ].map(([label, value], index) => (
-        <div key={index} className="flex flex-col sm:flex-row gap-2">
+        <div key={index} className="flex flex-col sm:flex-row">
           <p className="text-xs font-notoSemibold min-w-[8rem]">{label}:</p>
           <p className="text-xs break-words">{getValue(value)}</p>
         </div>
       ))}
 
-      <div className="flex flex-col sm:flex-row gap-2">
+      <div className="flex flex-col sm:flex-row">
         <p className="text-xs font-notoSemibold min-w-[8rem]">Lyric:</p>
         <div
-          className="text-xs leading-relaxed break-words max-h-48 overflow-auto px-3 py-2 rounded-m w-full"
+          className="text-xs leading-relaxed break-words rounded-m w-full"
           dangerouslySetInnerHTML={{
             __html: getValue(musicDetailInfo?.musicLyric),
           }}
