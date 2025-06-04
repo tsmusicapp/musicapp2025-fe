c91ee9273af5ee690b5f3243e859bb384a2638be
Mudassar Manzoor <mohsanbhatti864@gmail.com>
2025-06-04 10:47:10 +0500

    update cart icon in nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -135,7 +135,7 @@ export function Navbar() {
                       />
                     </Badge>
                   </Link>
-                  <ShoppingCartNav isScrolling={false} />
+                  <ShoppingCartNav isScrolling={true} />
                 </>
               ) : (
                 <>
